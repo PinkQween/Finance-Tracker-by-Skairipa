c9ee8569c96621de2b4887a46d7dd1581d0fc6eb
PinkQween <skairipa@pitchblack.tk>
2023-05-31 19:54:49 -0600

    Initial Commit

---
+++ Finance Tracker by Skairipa/Finance_Tracker_by_SkairipaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_Tracker_by_SkairipaApp.swift
+//  Finance Tracker by Skairipa
+//
+//  Created by Hanna Skairipa on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Finance_Tracker_by_SkairipaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
